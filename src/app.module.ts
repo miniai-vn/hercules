443c06545540e73ea4e7e9cf7461ca867523c26d
hgminh523 <hgminh523>
2025-03-10 08:50:27 +0700

    convert to host to .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -28,7 +28,7 @@ import { ShopsModule } from './shops/shops.module';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DATABASE_HOST,
-      port: 5432,
+      port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
       username: process.env.DATABASE_USER,
       password: process.env.DATABASE_PASSWORD,
       database: process.env.DATABASE_NAME,

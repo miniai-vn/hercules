0bb9ba4766a1cf82f9203e9f6ac2e4cdce34aad2
Thành Phan <th.phan1408@gmail.com>
2025-06-11 11:55:40 +0700

    comment tiktok

--- src/integration/integration.controller.ts
+++ src/integration/integration.controller.ts
@@ -1,4 +1,3 @@
-
 import { Controller, Get } from '@nestjs/common';
 import { IntegrationService } from './integration.service';
 import { ApiTags } from '@nestjs/swagger';
@@ -6,27 +5,25 @@ import { ApiTags } from '@nestjs/swagger';
 @ApiTags('Integration')
 @Controller('integration')
 export class IntegrationController {
+  constructor(private readonly integrationService: IntegrationService) {}
 
-    constructor(private readonly integrationService: IntegrationService) {}
+  //    @Get("authorize-tiktok")
+  //     async authorizeTikTok() {
+  //         return this.integrationService.authorizeTikTok();
+  //     }
 
-   @Get("authorize-tiktok")
-    async authorizeTikTok() {
-        return this.integrationService.authorizeTikTok();
-    }
+  //     @Get("tiktok/conversations")
+  //     async getConversations() {
+  //         return this.integrationService.getConversations();
+  //     }
 
-    @Get("tiktok/conversations")
-    async getConversations() {
-        return this.integrationService.getConversations();
-    }
+  //     @Get("tiktok/shop-info")
+  //     async getShopInfo() {
+  //         return this.integrationService.getAuthorizeShop();
+  //     }
 
-    @Get("tiktok/shop-info")
-    async getShopInfo() {
-        return this.integrationService.getAuthorizeShop();
-    }
-
-    @Get("tiktok/products")
-    async getProducts() {
-        return this.integrationService.getProducts();
-    }
-
+  //     @Get("tiktok/products")
+  //     async getProducts() {
+  //         return this.integrationService.getProducts();
+  //     }
 }

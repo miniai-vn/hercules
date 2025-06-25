1528384a5d34f214d838a61f5f26a8ce7fa9618b
Your Name <you@example.com>
2025-06-25 15:54:26 +0700

    add security kafka

--- src/kafka/kafka.producer.ts
+++ src/kafka/kafka.producer.ts
@@ -5,7 +5,7 @@ import { Kafka, Producer } from 'kafkajs';
 export class KafkaProducerService implements OnModuleInit, OnModuleDestroy {
   private kafka = new Kafka({
     clientId: 'kafka-app',
-    brokers: ['103.42.59.43:29092'],
+    brokers: [process.env.KAFKA_BROKERS || 'localhost:9092'],
     connectionTimeout: 10000,
     requestTimeout: 30000,
     retry: {
@@ -13,6 +13,12 @@ export class KafkaProducerService implements OnModuleInit, OnModuleDestroy {
       retries: 10,
       maxRetryTime: 30000,
       factor: 0.2,
+    },
+    ssl: false,
+    sasl: {
+      username: 'admin',
+      password: 'admin-secret',
+      mechanism: 'plain', // 'plain' or 'scram-sha-256'
     },
   });
   private producer = this.kafka.producer({
